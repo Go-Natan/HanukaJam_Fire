8e440d903f2aca6b4aa6a5c8a8ad2856915cf134
yairgalili <59600476+yairgalili@users.noreply.github.com>
2022-12-22 15:30:38 +0200

    slider and enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public Rigidbody2D _rb;
+    //movement speed in units per second
+    public float movementSpeed = 0.0005f;
     // Start is called before the first frame update
     void Start()
     {
+        _rb = GetComponent<Rigidbody2D>();
+
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = transform.position + new Vector3(0, -movementSpeed * Time.deltaTime, 0);
     }
 }
--- Assets/Scripts/Slider.cs
+++ Assets/Scripts/Slider.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Slider : MonoBehaviour
 {
+    [SerializeField] public float speed = 10f;
+    public float movementSpeed = 5f;
+
+    public Rigidbody2D _rb;
+    public SpriteRenderer _spriteRenderer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //get the Input from Vertical axis
+        float verticalInput = 0;
+        if (Input.GetKey(KeyCode.W)) // UpArrow	, DownArrow
+        {
+            verticalInput  = 1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            verticalInput  = -1;
+        }
+        //update the position
+        transform.position = transform.position + new Vector3(0, verticalInput * movementSpeed * Time.deltaTime, 0);
+
+        // _rb.velocity = new Vector2(_direction * speed, _rb.velocity.y);
         
     }
 }
